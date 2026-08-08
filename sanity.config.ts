e9a49454f70770cc7dde9df4b62d53f82f5569f6
vahidinline <vahidinline@gmail.com>
2026-08-08 06:59:39 -0400

    Add visionTool plugin to Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { schemaTypes } from './src/sanity/schemaTypes';
+import { visionTool } from '@sanity/vision';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'u09gju27';
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId,
   dataset,
   basePath: '/studio',
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
